6b4b253329ba79ca50a32eedcb1f0535bec12684
matteo ratti <matteoratti90@gmail.com>
2020-08-28 18:02:18 +0200

    1200px responsive

--- spectral/index.js
+++ spectral/index.js
@@ -1,12 +1,12 @@
 function openNav() {
   document.querySelector("body").style.background = "#2e3842";
   document.querySelector(".container").style.opacity = "0.35";
-  document.querySelector(".sidenav").style.width = "70%";
+  document.querySelector(".sidenav").classList.add("sidenav-width");
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.querySelector("body").style.background = "trasparent";
   document.querySelector(".container").style.opacity = "1";
-  document.querySelector(".sidenav").style.width = "0";
+  document.querySelector(".sidenav").classList.remove("sidenav-width");
 }
